8d2ff7126f39124f9506931131fcefe53756fff6
BenjaminKorady <Ben.Korady@gmail.com>
2023-12-08 06:47:05 +0000

    Update to calculate average of an array

--- HelloGit.java
+++ HelloGit.java
@@ -1,6 +1,13 @@
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        
+        int sum = 0;
+        int[] arr = {1, 2, 5, 7, 0, -3, 4};
+        for (int i = 0; i < arr.length; i++) {
+           sum += arr[i]; 
+        }
+
+        System.out.println("Average: " + (double) sum / arr.length);
     }
 }
 
