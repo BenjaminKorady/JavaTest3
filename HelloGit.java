4447f75d09c1d9a500e022022501283457c310d5
BenjaminKorady <Ben.Korady@gmail.com>
2023-12-08 06:49:06 +0000

    Refactor getAverage functionality

--- HelloGit.java
+++ HelloGit.java
@@ -1,13 +1,17 @@
 public class HelloGit {
     public static void main(String[] args) {
         
-        int sum = 0;
         int[] arr = {1, 2, 5, 7, 0, -3, 4};
+        System.out.println("Average: " + getAverage(arr));
+    }
+
+    private static double getAverage(int arr[]) {
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
            sum += arr[i]; 
         }
 
-        System.out.println("Average: " + (double) sum / arr.length);
+        return (double) sum / arr.length;
     }
 }
 
